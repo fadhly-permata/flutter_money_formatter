b479aee2c0157089de70ad011c5fae2b53f555f9
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2023-01-25 19:10:53 +0100

    bumped up sdk constraints, and updated dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## [0.0.4] - [25-01-2022]
+- bumped up sdk constraints, and updated dependencies
 ## [0.0.3] - [13-03-2021]
 - edited readme
 ## [0.0.2] - [13-03-2021]
--- test/money_formatter_test.dart
+++ test/money_formatter_test.dart
@@ -1,6 +1,5 @@
 // import 'package:flutter_test/flutter_test.dart';
 
-import 'package:money_formatter/money_formatter.dart';
 
 void main() {
   // test('adds one to input values', () {
